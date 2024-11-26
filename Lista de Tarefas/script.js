fd822dc81f2987708126317bb41776cfdcf5d635
Gabriel <gabrielistchuk@alunos.utfpr.edu.br>
2024-11-26 09:20:45 -0300

    mudança no tema e finalização

--- Lista de Tarefas/script.js
+++ Lista de Tarefas/script.js
@@ -1,8 +1,18 @@
-const tasks = [
-	{taskInfo: "Criar um documento HTML", check: true},
-	{taskInfo: "Estilizar pelo css", check: true},
-	{taskInfo: "Funcionalizar via javascript", check: true},
-]
+let tasks
+try {
+    tasks = JSON.parse(localStorage.getItem("tasks")) || [
+        { taskInfo: "Criar um documento HTML", check: true },
+        { taskInfo: "Estilizar pelo CSS", check: true },
+        { taskInfo: "Funcionalizar via JavaScript", check: true },
+    ]
+} catch (e) {
+    tasks = [
+        { taskInfo: "Criar um documento HTML", check: true },
+        { taskInfo: "Estilizar pelo CSS", check: true },
+        { taskInfo: "Funcionalizar via JavaScript", check: true },
+    ]
+}
+
 
 const ul = document.getElementsByTagName("ul")[0]
 const input = document.getElementById("taskInput")
@@ -19,28 +29,35 @@ const addTask = () => {
 		else{
 			tasks.push({taskInfo: input.value, check: false})
 			input.value = ""
+			input.placeholder = ""
 		}
 	}
 	else{
 		tasks.push({taskInfo: `Nova tarefa ${diff}`, check: false})
 		diff++
+		input.placeholder = ""
 	}
 	renderTasks()
 }
 
-const editLi = (taskInfo, span) => {
+const editTask = (taskInfo, span) => {
 	const task = tasks.find(t => t.taskInfo == taskInfo)
 	span.contentEditable = true
 
 	span.focus()
 
 	span.addEventListener("blur", () => {
-		task.taskInfo = span.textContent.trim()
+		if (tasks.find(t=>t.taskInfo == span.textContent) || span.textContent.trim() == ""){
+			task.taskInfo = taskInfo
+		}
+		else{
+			task.taskInfo = span.textContent.trim()
+		}
 		renderTasks()
 	})
 }
 
-const delLi = (taskInfo) => {
+const delTask = (taskInfo) => {
 	const task = tasks.findIndex(t => t.taskInfo == taskInfo)
 	if (task > -1){
 		tasks.splice(task, 1)
@@ -76,10 +93,13 @@ const createLi = (taskInfo, check) => {
 	edit.appendChild(document.createTextNode('create'))
 	const span = document.createElement('span')
 	checkbox.type = "checkbox"
-	checkbox.checked = check
+	checkbox.checked = check 
+	if (check){
+		span.classList.add("checked")
+	}
 	
-	del.addEventListener("click", ()=>{delLi(taskInfo)})
-	edit.addEventListener("click", ()=>{editLi(taskInfo, span)})
+	del.addEventListener("click", ()=>{delTask(taskInfo)})
+	edit.addEventListener("click", ()=>{editTask(taskInfo, span)})
 	checkbox.addEventListener("change", ()=>{checkHandle(taskInfo, checkbox)})
 	
 	li.appendChild(checkbox)
@@ -96,9 +116,53 @@ const updateProgressbar = () => {
 	
 	const totalTasks = tasks.length
 	const doneTasks = tasks.filter(task => task.check).length
+	if (totalTasks != 0){
+		pBar.value = (doneTasks / totalTasks) * 100
+	}
+	else{
+		pBar.value = 0
+	}
 	
-	pBar.value = (doneTasks / totalTasks) * 100
 }
 
-updateProgressbar()
+const themeToggle = document.querySelector('.tema')
+const body = document.body
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark')
+
+    if (body.classList.contains('dark')) {
+        localStorage.setItem('theme', 'dark')
+    } else {
+        localStorage.setItem('theme', 'light')
+    }
+})
+
+window.addEventListener('load', () => {
+    const savedTheme = localStorage.getItem('theme')
+    if (savedTheme === 'dark') {
+        body.classList.add('dark')
+    }
+})
+
+
+function showContatos() {
+	document.getElementById('card').style.display = "flex"
+}
+
+function hideContatos(){
+	document.getElementById('card').style.display = "none"
+}
+
+document.addEventListener('keydown', function (event) {
+	if (event.ctrlKey && event.key === 'h') {
+	  event.preventDefault(); // Evita a função padrão do navegador (como abrir o histórico)
+	  showContatos(); // Abre a tela contatos
+	}
+  })
+
+window.addEventListener("beforeunload", ()=>{
+	localStorage.setItem("tasks", JSON.stringify(tasks))
+})
+
 renderTasks()
